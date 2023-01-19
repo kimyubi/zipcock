55da439dbcc2598a1ec7d9cedb4d8549e0b9505f
kimyubi <ieieie0419@gmail.com>
2023-01-19 15:43:17 +0900

    Initial commit

---
+++ src/main/java/com/umc/zipcock/ZipcockApplication.java
@@ -0,0 +1,13 @@
+package com.umc.zipcock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipcockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipcockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/umc/zipcock/ZipcockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.zipcock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipcockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
