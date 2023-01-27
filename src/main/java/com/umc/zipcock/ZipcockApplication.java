eecd0727e626d31bfffd2c9764bc5d63b740477c
kimyubi <ieieie0419@gmail.com>
2023-01-27 12:01:14 +0900

    Feat #15: queryDsl 사용을 위한 build.gradle 설정
    - build.gradle 설정 후 Reload Gradle Projdct 를 실행한 다음,
      Gradle Tasks -> compileQuerydsl 을 실행하면 build/generated/querydsl
      경로에 Project Entity 들의 QClass 가 생성된 것을 확인할 수 있다.

--- src/main/java/com/umc/zipcock/ZipcockApplication.java
+++ src/main/java/com/umc/zipcock/ZipcockApplication.java
@@ -2,7 +2,6 @@ package com.umc.zipcock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class ZipcockApplication {
